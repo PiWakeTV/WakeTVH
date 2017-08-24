f3785db727bbee8200768bf7aa5e5a454d81b03d
PiWakeTV <aPCqb1MTm5Mokp0VTem1>
2017-08-24 22:28:39 +0200

    Removed unused imports

--- src/main/java/org/piheadend/config/SchedulerConfig.java
+++ src/main/java/org/piheadend/config/SchedulerConfig.java
@@ -1,16 +1,9 @@
 package org.piheadend.config;
 
-import org.piheadend.services.recording.RecordingInfo;
 import org.piheadend.services.recording.RecordingInfoService;
-import org.piheadend.services.storage.StorageService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.util.List;
 
 /**
  * Configuration for the schedulers.
